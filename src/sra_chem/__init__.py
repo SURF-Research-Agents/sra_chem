e273efaeb9c0e0fa58d936d0ff142d69d44cfd50
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2026-07-20 09:45:16 +0200

    init repo

---
+++ src/sra_chem/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about sra_chem."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Nicolas Renaud"
+__email__ = "nicolas.gm.renaud@gmail.com"
+__version__ = "0.1.0"
