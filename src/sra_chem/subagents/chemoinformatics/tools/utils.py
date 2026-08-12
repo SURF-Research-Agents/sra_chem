62a229a9de01edea90866f98acce4c9f0711d568
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2026-08-12 17:30:59 +0200

    removed ase tools [not used atm]

--- src/sra_chem/subagents/chemoinformatics/tools/utils.py
+++ src/sra_chem/subagents/chemoinformatics/tools/utils.py
@@ -8,3 +8,20 @@ def _resolve_path(path: str) -> str:
         os.makedirs(log_dir, exist_ok=True)
         return os.path.join(log_dir, path)
     return path
+
+
+from pydantic import BaseModel, Field
+from typing import List, Optional, Union
+
+
+class AtomsData(BaseModel):
+    """AtomsData object inherited from Pydantic BaseModel. Used to store atomic data (from ASE Atoms object or QCElemental Molecule object) that cannot be parsed via LLM Schema."""
+
+    numbers: List[int] = Field(..., description="Atomic numbers")
+    positions: List[List[float]] = Field(..., description="Atomic positions")
+    cell: Optional[Union[List[List[float]], None]] = Field(
+        default=None, description="Cell vectors or None"
+    )
+    pbc: Optional[Union[List[bool], None]] = Field(
+        default=None, description="Periodic boundary conditions or None"
+    )
